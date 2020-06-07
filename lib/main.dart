b181bb6a56f97484332f23fe7d5c1ea935f27689
idnasirasira <idnasirasira@gmail.com>
2020-06-08 01:48:39 +0700

    learn flutter BLoC State Management no library.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapp/bloc_state_management/color_bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,16 +9,54 @@ class MyApp extends StatefulWidget{
 }
 
 class _MainAppState extends State<MyApp>{
+  ColorBloc bloc = ColorBloc();
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: () {
+                bloc.eventSink.add(ColorEvent.to_red);
+              },
+              backgroundColor: Colors.red,
+            ),
+            SizedBox(width: 10),
+            FloatingActionButton(
+              onPressed: () {
+                bloc.eventSink.add(ColorEvent.to_blue);
+              },
+              backgroundColor: Colors.blue,
+            )
+          ],
+        ),
         appBar: AppBar(
             title: Text("BLoC State Management")
         ),
         body: Center(
-            child: Text("Ini Text")
+            child: StreamBuilder(
+              stream: bloc.stateStream,
+              initialData: Colors.red,
+              builder: (context, snapshot) {
+                return AnimatedContainer(
+                    width: 100,
+                    height: 100,
+                    color: snapshot.data,
+                    duration: Duration(seconds: 1)
+                );
+              },
+            )
         ),
       ),
     );
